7c8c4b999c5412e111becb695e557d4ebd12953f
이숙영 <22sook00@gmail.com>
2021-11-16 17:25:07 +0900

    Add reservation name

--- src/features/reservationSlice.ts
+++ src/features/reservationSlice.ts
@@ -1,16 +1,25 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice,PayloadAction } from '@reduxjs/toolkit';
 
-const initialState = {
+//value 의 타입을 지정해줘야 한다.
+interface ReservationState {
+  //array 형태 안에있는 스트링의 타입.
+  value : string[]
+}
+
+const initialState:ReservationState = {
   value : [],
-
 };
 
 export const reservationSlice = createSlice({
   name : 'reservation',
   initialState,
-  //reducers : for update state
-  reducers : {}
-    
-})
+  //reducers : for update particular state
+  reducers : {
+    addReservation : (state,action:PayloadAction<string> ) => {
+      state.value.push(action.payload)
+    },
+  }, 
+});
 
+export const {addReservation} = reservationSlice.actions
 export default reservationSlice.reducer;
